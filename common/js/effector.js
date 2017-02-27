60db6a933ca12218324f23e82bfc41c050deb880
Masashi Morita <masashi.morita@itdcanada.ca>
2017-02-26 21:57:18 -0800

    :sparkles: Implement SlideIn effects and add delay before effects start

--- common/js/effector.js
+++ common/js/effector.js
@@ -16,38 +16,51 @@ Effector.prototype = {
     elementIndex: 0,
     nodeIndex: 0,
     isEffectInitialized: false,
+    delay: 0,
 
     initialize: function (option) {
+        if (typeof option.delay !== 'number' || option.delay < 0) {
+            console.log('Error : Delay is not vaild...');
+            return;
+        }
         this.option = option;
         this.elementLength = option.elements.length;
         this.effectLength = this.interval * (1 / this.step);
+        this.delay = option.delay;
         var isValid = true;
         for (var i = 0; i < this.elementLength; i++) {
             var obj = {};
+            obj.useCustomDirection = (typeof option.elements[i].useCustomDirection === 'boolean') ? option.elements[i].useCustomDirection : false;
             var toLength, fromLength;
-            var isValidDestination = (Array.isArray(option.elements[i].to) && Array.isArray(option.elements[i].from)) ? true : false;
-            if (isValidDestination) {
-                toLength = option.elements[i].to.length;
-                fromLength = option.elements[i].from.length;
-            }
+            var isValidDestination = (Array.isArray(option.elements[i].to)) ? true : false;
+            var isValidFrom = (Array.isArray(option.elements[i].from)) ? true : false;
+            if (isValidDestination) toLength = option.elements[i].to.length;
+            if (isValidFrom) fromLength = option.elements[i].from.length;
             var nodes = document.querySelectorAll(option.elements[i].selector);
             var nodeLength = nodes.length;
             for (var j = 0; j < nodeLength; j++) {
                 nodes[j].initPos = this.getPosition(nodes[j]);
-                nodes[j].style.opacity = 0.0;
                 if (isValidDestination) {
-                    nodes[j].from = option.elements[i].from[j];
                     nodes[j].to = option.elements[i].to[j];
-                    if (j + 1 === toLength || j + 1 === fromLength) {
+                    if (j + 1 === toLength) {
                         isValidDestination = false;
                     }
                 }
+                if (isValidFrom) {
+                    nodes[j].from = option.elements[i].from[j];
+                    if (j + 1 === fromLength) {
+                        isValidFrom = false;
+                    }
+                }
+                nodes[j].style.cssText = ''.concat(
+                    'opacity : 0.0;'
+                );
             }
             if (typeof this[option.elements[i].func] !== 'function') {
                 isValid = false;
                 break;
             }
-            obj.func = option.elements[i].func;
+            obj.func = option.elements[i].func.toLowerCase();
             obj.showTogether = (typeof option.elements[i].showTogether === 'boolean') ? option.elements[i].showTogether : true;
             obj.nodes = nodes;
             this.elements.push(obj);
@@ -57,7 +70,7 @@ Effector.prototype = {
             console.log('Effect Function is invalid');
         }
 
-        this.start();
+        window.setTimeout(this.start.bind(this), this.delay);
     },
 
     getPosition: function (element) {
@@ -82,6 +95,42 @@ Effector.prototype = {
         return {x: x, y: y};
     },
 
+    getDirection: function (direction, isDistination, size) {
+        direction = direction.toLowerCase();
+        size = {x: (size.x / window.innerWidth) * 100, y: (size.y / window.innerHeight) * 100};
+        var adjustment = (isDistination) ? {x: 0, y: 0} : size;
+
+        switch (direction) {
+            case 'topleft' :
+                return {x: 0 - adjustment.x, y: 0 - adjustment.y};
+            case 'middleleft' :
+                return {x: 0 - adjustment.x, y: 50 - (size.y / 2.0)};
+            case 'bottomleft' :
+                return {x: 0 - adjustment.x, y: 100};
+            case 'bottommiddle' :
+                return {x: 50 - (size.x / 2.0), y: 100};
+            case 'bottomright' :
+                return {x: 100, y: 100};
+            case 'middleright' :
+                return {x: 100, y: 50 - (size.y / 2.0)};
+            case 'topright' :
+                return {x: 100, y: 0 - adjustment.y};
+            case 'topmiddle' :
+                return {x: 50 - (size.x / 2.0), y: 0 - adjustment.y};
+            case  'center' :
+                return {x: 50 - (size.x / 2.0), y: 50 - (size.y / 2.0)};
+            default:
+                return {x: 0, y: 0};
+        }
+    },
+
+    calcPosition: function (playback, to, from) {
+        var x = (to.x >= from.x) ? to.x * playback : (from.x - from.x * playback);
+        var y = (to.y >= from.y) ? to.y * playback : (from.y - from.y * playback);
+
+        return {x: x, y: y};
+    },
+
     start: function () {
         this.startedAt = 0;
         this.elementIndex = 0;
@@ -92,6 +141,13 @@ Effector.prototype = {
         clearInterval(this.intervalId);
         this.intervalId = 0;
         this.isEffectInitialized = false;
+    },
+
+    reset: function (elements) {
+        var length = elements.length;
+        for (var elIndex = 0; elIndex < length; elIndex++) {
+            elements[elIndex].style.cssText = '';
+        }
     },
 
     update: function () {
@@ -108,18 +164,20 @@ Effector.prototype = {
 
         if (this.startedAt > 1.0) {
             if (this.elements[this.elementIndex].showTogether) {
+                this.reset(this.elements[this.elementIndex].nodes);
                 this.elementIndex++;
             } else {
+                this.reset([this.elements[this.elementIndex].nodes[this.nodeIndex]])
                 this.nodeIndex++;
-                if(this.nodeIndex >= this.elements[this.elementIndex].nodes.length){
+                if (this.nodeIndex >= this.elements[this.elementIndex].nodes.length) {
                     this.elementIndex++;
                     this.nodeIndex = 0;
                 }
             }
 
-            if(this.elementIndex >= this.elementLength){
+            if (this.elementIndex >= this.elementLength) {
                 this.stop();
-            }else{
+            } else {
                 this.startedAt = 0;
                 this.isEffectInitialized = false;
             }
@@ -129,8 +187,27 @@ Effector.prototype = {
     fadein: function (playback, element) {
         if (!this.isEffectInitialized) {
             element.style.opacity = 0.0;
+            return;
         }
         element.style.opacity = playback;
+    },
+
+    slidein: function (playback, element) {
+        if (!this.isEffectInitialized) {
+            if (!this.elements[this.elementIndex].useCustomDirection) {
+                var size = {x: element.offsetWidth, y: element.offsetHeight};
+                element.from = this.getDirection(element.from, false, size);
+                element.to = element.initPos;
+            }
+            element.style.cssText += ''.concat(
+                'position : absolute;', 'left : ' + element.from.x + '%;', 'top : ' + element.from.y + '%;', 'opacity : 1.0;'
+            );
+            return;
+        }
+        var pos = this.calcPosition(playback, element.to, element.from);
+        element.style.cssText += ''.concat(
+            'left : ' + pos.x + '%;', 'top : ' + pos.y + '%;'
+        );
     },
 };
 
